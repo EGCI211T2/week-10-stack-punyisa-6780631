b98cbfd59fba11666c7e64146a10843c6e68f083
punyisa-6780631 <punyisa.pan@student.mahidol.edu>
2025-11-23 05:15:16 +0000

    pp

--- main.cpp
+++ main.cpp
@@ -5,22 +5,23 @@ using namespace std;
 
 int main(int argc, char **argv){
     Stack s;
-     s.push(5);
+
+    /* s.push(5);
       s.push(1);
       s.push(7);
       s.pop();
       s.push(6);
-      s.pop();
+      s.pop();*/
 
  /*
   Exercise 1*/
-    /*
+    
     int i;
     for(i=1;i<argc;i++){
-      //  if(  x)  s.pop();
+        if(argv[i][0] == 'x')  s.pop(); //strcmp(argv[i], "x") == 0 for other option. Also don't forget to include <cstring>
         else s.push(atoi(argv[i]));
     }
-     */
+    //this will pop the number b4 x then in the end it will clear all the remainding numbers from the back
 
 
 
