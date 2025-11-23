ab9266211b693fb36a486e18fa31f62eb34f61fb
punyisa-6780631 <punyisa.pan@student.mahidol.edu>
2025-11-23 05:13:09 +0000

    pp

--- stack.h
+++ stack.h
@@ -15,32 +15,42 @@ public:
 
 void Stack::push(int x){
   NodePtr new_node=new NODE(x);
-  if(new_node){
-            // Left missing for exercises…
+  if(new_node/*if new node can be created succesfully*/){
+    new_node->set_next(top); //set the new one to the previous top
+    top=new_node;
+    size++;
    }
- 
-         // Left missing for exercises…
-    
+   else cout<<"Not enough memory"<<endl;
 }
 
 int Stack::pop(){
         NodePtr t=top;
-        int value;
-        value=t->get_value();
-    // Left missing part for exercises
-        delete t;
-        return value;
+        if(t!=NULL){
+            int value;
+            value=t->get_value();
+            top = t->get_next(); // move top to the next one and decrease the size
+            --size;
+            delete t;
+            return value;
+        }
+        
+    cout<<"Empty stack"<<endl;
+    return -1;
 	//be careful of the empty stack!!!
     }
 
 Stack::Stack(){
-    //initialize stack
+    top=NULL;
+    size=0;//initialize stack
     
 }
 Stack::~Stack(){
     //delete all remaning stack (i.e. pop all) 
-    
+    cout<<"Clear the stack"<<endl;
+    int n=size;
+    while(n>0) {
+        pop();
+        n--;
+    }
 }
-
-
 #endif
